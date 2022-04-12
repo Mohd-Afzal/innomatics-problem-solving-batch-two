c299e2d86ad857bea25a87df866c098c46b0d624
Mohd-Afzal <mohammedafsal878@gmail.com>
2022-04-12 09:32:40 +0530

    Ontroduction to Recursion

--- src/recursion/RecursionBasics.java
+++ src/recursion/RecursionBasics.java
@@ -5,7 +5,26 @@ import java.util.Scanner;
 public class RecursionBasics {
     static Scanner s = new Scanner(System.in);
 
+    public static int fact(int n) {
+//        System.out.println("New input is : " + n);
+
+        // base condition/terminating condition/end point
+        if (n == 0) {
+            return 1;
+        }
+
+        // Induction Hypothesis
+        int smallAns = fact(n - 1); // Assuming that I will get the correct answer
+
+        // Induction Step
+        int myAns = n * smallAns;
+        return myAns;
+    }
+
     public static void main(String[] args) {
+
+        int ans = fact(4);
+        System.out.println(ans);
 
     }
 }
